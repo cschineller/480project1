3488bb201f811f4446d89351ddb2dcafe54cc514
Callie Schineller <cschineller@colgate.edu>
2020-09-21 14:20:21 -0400

    Works in one direction but not the other

--- SecureMessaging.py
+++ SecureMessaging.py
@@ -21,18 +21,19 @@ from Crypto.Cipher import AES
 
 QUEUE_LENGTH = 1
 SEND_BUFFER_SIZE = 2048
-C_PUBKEY = 0
-S_PUBKEY = 0
-C_SHAREDKEY = 0
-S_SHAREDKEY = 0 
 SERVER = 0
-
 
 class SecureMessage:
 
     def __init__(self, server_ip=None, server_port=None):
         """Initialize SecureMessage object, create & connect socket,
            do key exchange, and start send & receive loops"""
+
+        self.C_PUBKEY = 0
+        self.S_PUBKEY = 0
+        self.C_SHAREDKEY = 0
+        self.S_SHAREDKEY = 0 
+        self.SPLITTER = "splt"
 
         # create IPv4 TCP socket
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -70,7 +71,7 @@ class SecureMessage:
     def recv_loop(self):
         """Loop to receive and print messages"""
         while True:
-            recv_msg = self.s.recv(SEND_BUFFER_SIZE).decode()
+            recv_msg = self.s.recv(SEND_BUFFER_SIZE).decode("ISO-8859-1")
             if recv_msg:
                 message = self.process_received_message(recv_msg)
                 sys.stdout.write("\t" + message + "\n")
@@ -83,64 +84,70 @@ class SecureMessage:
         c = pyDH.DiffieHellman() #client
         s = pyDH.DiffieHellman() #server
 
-        C_PUBKEY = c.gen_public_key() #global variable
-        S_PUBKEY = s.gen_public_key() #global variable
+        self.C_PUBKEY = c.gen_public_key() #global variable
+        self.S_PUBKEY = s.gen_public_key() #global variable
 
         #client sends their public key to server
-        self.s.send((C_PUBKEY.to_bytes(SEND_BUFFER_SIZE, sys.byteorder)))
+        self.s.send(str(self.C_PUBKEY).encode("ISO-8859-1"))
         c_pubkey = self.s.recv(SEND_BUFFER_SIZE)
 
         #server sends their public key to client
-        self.s.send((C_PUBKEY.to_bytes(SEND_BUFFER_SIZE, sys.byteorder)))
+        self.s.send(str(self.S_PUBKEY).encode("ISO-8859-1"))
         s_pubkey = self.s.recv(SEND_BUFFER_SIZE)
 
         #generate shared keys using what they received from each other
-        C_SHAREDKEY = c.gen_shared_key(s_pubkey)
-        S_SHAREDKEY = s.gen_shared_key(c_pubkey) 
+        self.C_SHAREDKEY = c.gen_shared_key(int(s_pubkey.decode("ISO-8859-1")))
+        self.S_SHAREDKEY = s.gen_shared_key(int(c_pubkey.decode("ISO-8859-1"))) 
         
 
     def process_user_input(self, user_input):
         
         #check if we're dealing with the server or the client to know which key to use
         if SERVER: 
-            ciphertxt = AES.new(S_PUBKEY[:32], AES.MODE_EAX) 
+            ciphertxt = AES.new(bytes(self.S_SHAREDKEY, encoding="ISO-8859-1")[:16], AES.MODE_EAX) 
         else:    
-            ciphertxt = AES.new(C_PUBKEY[:32], AES.MODE_EAX)
+            ciphertxt = AES.new(bytes(self.C_SHAREDKEY, encoding="ISO-8859-1")[:16], AES.MODE_EAX)
             
-        nonce = Crypto.nonce
-        splitter = "splt".encode("ISO-8859-1")
+        nonce = ciphertxt.nonce
+        self.SPLITTER = "splt".encode("ISO-8859-1")
+        #print("SPLITTER TYPE IS:", type(self.SPLITTER))
         ciphertxt, tag = ciphertxt.encrypt_and_digest(user_input)
 
-        return nonce+splitter+ciphertxt+splitter+tag
+        return nonce+self.SPLITTER+ciphertxt+self.SPLITTER+tag
 
     def process_received_message(self, recv_msg):
-
         #split the message into its parts using the splitter
-        array = recv_msg.split("splt".decode("ISO-8859-1"))
-        nonce = array[0]
+        #recv_msg = recv_msg.encode("ISO-8859-1")
+        array = recv_msg.split(str(self.SPLITTER))
+        print("ARRAY LENGTH:", len(array))
+        nonce = bytes(array[0], encoding="ISO-8859-1")
         ciphertxt = array[1]
         tag = array[2]
 
         if SERVER:
-            cipher = AES.new(S_PUBKEY[:32], AES.MODE_EAX, nonce=nonce)
+            cipher = AES.new(bytes(self.S_SHAREDKEY[:16], encoding="ISO-8859-1"), AES.MODE_EAX, nonce=nonce)
         else:
-            cipher = AES.new(C_PUBKEY[:32], AES.MODE_EAX, nonce=nonce)
+            cipher = AES.new(bytes(self.C_SHAREDKEY[:16], encoding="ISO-8859-1"), AES.MODE_EAX, nonce=nonce)
+
 
         #decrypt the ciphertxt using the cipher
-        plaintxt = cipher.decrypt(ciphertxt)
+        plaintxt = cipher.decrypt(bytes(ciphertxt, encoding="ISO-8859-1"))
+
+        #print("PLAINTXT IS OF TYPE:", type(plaintxt))
 
         #check for integrity/authenticity
         try:
-            cipher.verify(tag)
+            cipher.verify(bytes(tag, encoding="ISO-8859-1"))
 
         except ValueError:
             print("Key incorrect or message corrupted")
             os._exit(0)
 
-        return plaintxt
+        return plaintxt.decode("ISO-8859-1")
 
 
 def main():
+    global SERVER
     """Parse command-line arguments and start client/server"""
     # too few arguments
     if len(sys.argv) < 2:
